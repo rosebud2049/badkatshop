fe59c6dd1b5f627b268d0dc983388a0147fb11ef
Administrator <raphael.palerme@epitech.eu>
2020-10-11 23:51:42 +0200

    add image field

--- pages/illustrations/_id/index.vue
+++ pages/illustrations/_id/index.vue
@@ -2,9 +2,6 @@
   <div class="flex justify-center m-6">
     <div v-if="this.illustration !== null">
       <div class="flex flex-col items-center border rounded-lg bg-gray-100">
-        <div class="w-full bg-white rounded-lg flex justify-center">
-          <img :src="illustration.image[0].formats.small.url" width="375" />
-        </div>
         <div class="w-full bg-white rounded-lg flex justify-center"></div>
         <div class="w-full p-5 flex flex-col justify-between">
           <div>
@@ -52,9 +49,6 @@ export default {
             title
             description
             price
-            image {
-              formats
-            }
           }
         }
       `,
