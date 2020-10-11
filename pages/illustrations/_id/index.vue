9f6ab8e946f43bf73b595bde649cc4932d6bd4f8
Administrator <raphael.palerme@epitech.eu>
2020-10-11 20:32:38 +0200

    front

---
+++ pages/illustrations/_id/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="flex justify-center m-6">
+    <div v-if="this.illustration !== null">
+      <div class="flex flex-col items-center border rounded-lg bg-gray-100">
+        <div class="w-full bg-white rounded-lg flex justify-center"></div>
+        <div class="w-full p-5 flex flex-col justify-between">
+          <div>
+            <h4
+              class="mt-1 font-semibold text-lg leading-tight truncate text-gray-700"
+            >
+              {{ illustration.title }}
+            </h4>
+            <div class="mt-1 text-gray-600">{{ illustration.description }}</div>
+          </div>
+
+          <button
+            class="snipcart-add-item mt-4 bg-white border border-gray-200 d hover:shadow-lg text-gray-700 font-semibold py-2 px-4 rounded shadow"
+            :data-item-id="illustration.id"
+            :data-item-price="illustration.price"
+            :data-item-url="`${storeUrl}${this.$route.fullPath}`"
+            :data-item-description="illustration.description"
+            :data-item-name="illustration.title"
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  data() {
+    return {
+      illustration: null,
+      storeUrl: process.env.storeUrl,
+      fullPath: this.$route.fullPath,
+    };
+  },
+  apollo: {
+    illustration: {
+      query: gql`
+        query illustration($id: ID!) {
+          illustration(id: $id) {
+            id
+            title
+            description
+            price
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.$route.params.id,
+        };
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div
+      class="m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4"
+    >
+      <div
+        v-for="p in illustrations"
+        :key="p.id"
+        class="border rounded-lg bg-gray-100 hover:shadow-lg"
+      >
+        <nuxt-link :to="`/illustrations/${p.id}`">
+          <div class="rounded-t-lg bg-white pt-2 pb-2"></div>
+          <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+            <h4
+              class="mt-1 font-semibold text-base leading-tight truncate text-gray-700"
+            >
+              {{ p.title }}
+            </h4>
+            <div class="mt-1 text-sm text-gray-700">{{ p.description }}</div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+
+export default {
+  data() {
+    return {
+      illustrations: [],
+      storeUrl: process.env.storeUrl,
+    };
+  },
+  apollo: {
+    illustrations: gql`
+      query illustrations {
+        illustrations {
+          id
+          title
+          description
+        }
+      }
+    `,
+  },
+};
+</script>
+
+<style>
+.crop {
+  width: 180px;
+  height: 180px;
+}
+</style>
